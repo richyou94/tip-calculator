364abb17b6c80d0bf7ca01f6b95e2207588cc4bf
Richard You <richardyou94@gmail.com>
2023-02-09 19:45:19 -0800

    add question for how many people split

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ total_bill = float(input('What was the total bill? $'))
 
 percentage_tip = input('What percentage tip would you like to give? 10, 12, 15, or 18? ')
 
+people_split = int(input('How many people to split the bill? '))
+
 print(total_bill)
 print(percentage_tip)
 
+result = 0
 
+print(f'Each person should pay: ${result}')
