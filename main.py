4d7e21d74c4aaad782f87d82e9be75c6831cd1ac
Richard You <richardyou94@gmail.com>
2023-02-09 19:49:19 -0800

    add calculation for adding total bill with tip

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ print('Welcome to the tip calculator\n')
 
 total_bill = float(input('What was the total bill? $'))
 
-percentage_tip = input('What percentage tip would you like to give? 10, 12, 15, or 18? ')
+percentage_tip = int(input('What percentage tip would you like to give? 10, 12, 15, or 18? '))
 
 people_split = int(input('How many people to split the bill? '))
 
-print(total_bill)
-print(percentage_tip)
+bill_include_tip = total_bill * (1 + (percentage_tip / 100))
 
-result = 0
+print(round(bill_include_tip, 2))
 
-print(f'Each person should pay: ${result}')
+
+# result = 0
+
+# print(f'Each person should pay: ${result}')
+
