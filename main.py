17d7b869a877a6a4aec61742a6039fe25b61d63e
Richard You <richardyou94@gmail.com>
2023-02-09 19:42:19 -0800

    add input message for percentage tip

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ print('Welcome to the tip calculator\n')
 
 total_bill = float(input('What was the total bill? $'))
 
+percentage_tip = input('What percentage tip would you like to give? 10, 12, 15, or 18? ')
+
 print(total_bill)
-print(type(total_bill))
+print(percentage_tip)
+
+
