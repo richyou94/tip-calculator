6e0913483bb5b49173b50f83e9b29823f1503982
Richard You <richardyou94@gmail.com>
2023-02-09 19:54:51 -0800

    add last comment of what to improve

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ people_split = int(input('How many people to split the bill? '))
 bill_include_tip = total_bill * (1 + (percentage_tip / 100))
 
 bill_per_person = bill_include_tip / people_split
+# It will be better if I can limit the result into two decimals
 result = round(bill_per_person, 2)
 
 print(f'Each person should pay: ${result}')
